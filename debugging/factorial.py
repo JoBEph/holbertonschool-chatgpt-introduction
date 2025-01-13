de70b549487cadbc56d06c55330877710825c00d
root <root@JordanPro.localdomain>
2025-01-13 10:44:53 +0100

    test 3

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n in each iteration
+        n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
-
