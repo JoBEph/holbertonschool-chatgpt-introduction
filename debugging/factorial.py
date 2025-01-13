99b9d8c06fde32a8fc3dcbab0bc8855d17f8821d
root <root@JordanPro.localdomain>
2025-01-13 10:21:33 +0100

    test

---
+++ debugging/factorial.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    if n == 0:  # Handle the edge case of 0!
+        return 1
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n to avoid infinite loop
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
